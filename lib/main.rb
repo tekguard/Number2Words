cfd6e2e21f6f9f595327627d572c260c5a966d60
Kanna <rajesh.kannan@msystechnologies.com>
2019-02-21 01:10:19 +0530

    Block divied

--- lib/main.rb
+++ lib/main.rb
@@ -5,7 +5,10 @@ class Main
     def parse(input)
       input = input.to_s
       ph_regex = /\A(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-.]\d{4})\z/
-      return 'Please enter valid input' if input.nil? || input.length != 10 || !ph_regex.match?(input) || /0/.match?(input)
+      if input.nil? || input.length != 10 || !ph_regex.match?(input) || /0/.match?(input)
+        return 'Please enter valid input'
+      end
+
       num_map = {
         2 => %w[a b c],
         3 => %w[d e f],
@@ -16,13 +19,34 @@ class Main
         8 => %w[t u v],
         9 => %w[w x y z]
       }
-      entries = input.to_s.chars.map{ |number| num_map[number.to_i] }
+      entries = input.to_s.chars.map { |number| num_map[number.to_i] }
+
       words = {}
       File.foreach('./public/english-words.txt') do |line|
         words[line.length] = words.fetch(line.length, []) << line.chop.to_s.downcase
       end
+
+      begin
+        matches = match_opr(entries, words)
+      rescue MatchOperation
+        raise 'Operation Failed'
+      end
+
+      begin
+        results = combination_opr(entries, words, matches)
+      rescue CombinationOpr
+        raise 'Operation Failed'
+      end
+
+      results
+    end
+
+    private
+
+    def match_opr(entries, words)
       matches = {}
       keys = entries.length - 1
+
       (1..keys).to_a.each do |i|
         match1 = entries[0..i]
         next if match1.length < 3
@@ -34,8 +58,15 @@ class Main
         next if combination1.nil?
 
         combination2 = match2.shift.product(*match2).map(&:join)
-        matches[i] = [(combination1 & words[i+2]), (combination2 & words[keys - i +1])]
+        s1 = (combination1 & words[i + 2])
+        s2 = (combination2 & words[keys - i + 1])
+        matches[i] = [s1, s2]
       end
+
+      matches
+    end
+
+    def combination_opr(entries, words, matches)
       results = []
       matches.each do |_, combinations|
         next if combinations.first.nil? || combinations.last.nil?
@@ -44,7 +75,9 @@ class Main
           results << combo_words
         end
       end
-      results << (entries.shift.product(*entries).map(&:join) & words[11]).join(', ')
+      results << (
+        entries.shift.product(*entries).map(&:join) & words[11]
+      ).join(', ')
       results
     end
   end
